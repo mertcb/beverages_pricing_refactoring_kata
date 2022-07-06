787ba7f8224c7cd7c6e000bb3e97c2ab2046a036
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-07-06 16:22:36 +0200

    Moving hardcoding into constant

--- src/test/java/beverages/WithCinammonTest.java
+++ src/test/java/beverages/WithCinammonTest.java
@@ -12,12 +12,13 @@ public class WithCinammonTest {
     public static final double REGULAR_TEA_PRICE = 1.5;
     public static final double CINNAMON_OVERHEAD = 0.05;
     private static final double REGULAR_HOT_CHOCOLATE_PRICE = 1.45;
+    public static final double PRECISION = 0.001;
 
     @Test
     public void adds_fifteen_cents_to_original_price()
     {
         HotChocolate hotChocolate = new HotChocolate();
         WithCinnamon hotChocolateWithCinnamon = new WithCinnamon(hotChocolate);
-        assertThat(REGULAR_HOT_CHOCOLATE_PRICE + CINNAMON_OVERHEAD, is(closeTo(hotChocolateWithCinnamon.price(), 0.001 )) );
+        assertThat(REGULAR_HOT_CHOCOLATE_PRICE + CINNAMON_OVERHEAD, is(closeTo(hotChocolateWithCinnamon.price(), PRECISION)) );
     }
 }
--- src/test/java/beverages/WithCreamTest.java
+++ src/test/java/beverages/WithCreamTest.java
@@ -13,12 +13,13 @@ public class WithCreamTest {
     public static final double REGULAR_TEA_PRICE = 1.5;
     public static final double CREAM_OVERHEAD = 0.15;
     private static final double REGULAR_HOT_CHOCOLATE_PRICE = 1.45;
+    public static final double PRECISION = 0.001;
 
     @Test
     public void adds_fifteen_cents_to_original_price()
     {
         HotChocolate hotChocolate = new HotChocolate();
         WithCream hotChocolateWithCream = new WithCream(hotChocolate);
-        assertThat(REGULAR_HOT_CHOCOLATE_PRICE + CREAM_OVERHEAD, is(closeTo(hotChocolateWithCream.price(), 0.001 )) );
+        assertThat(REGULAR_HOT_CHOCOLATE_PRICE + CREAM_OVERHEAD, is(closeTo(hotChocolateWithCream.price(), PRECISION)) );
     }
 }

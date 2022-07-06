82e935d871ed61deca26f427f2e3b4e44168f540
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-07-06 16:27:20 +0200

    Moving hardcoding into constant

--- src/test/java/unit_tests/BeveragesPricingTest.java
+++ src/test/java/unit_tests/BeveragesPricingTest.java
@@ -10,48 +10,59 @@ import static org.hamcrest.Matchers.closeTo;
 public class BeveragesPricingTest {
 
     public static final double REGULAR_COFFE_PRICE = 1.20;
+    public static final double REGULAR_TEA_PRICE = 1.50;
+    public static final double HOT_CHOCOLATE_REGULAR_PRICE = 1.45;
+
     public static final double CREAM_OVERHEAD = 0.15;
     public static final double MILK_OVERHEAD = 0.10;
 
+
     @Test
     public void computes_coffee_price() {
-        Priceable coffee = new Coffee();
+        Priceable coffee = BeverageMachine.coffee().make();
         assertThat(coffee.price(), is(closeTo(REGULAR_COFFE_PRICE, 0.001)));
     }
 
     @Test
     public void computes_tea_price() {
-        Priceable tea = new Tea();
-        assertThat(tea.price(), is(closeTo(1.50, 0.001)));
+        Priceable tea = BeverageMachine.tea().make();
+        assertThat(tea.price(), is(closeTo(REGULAR_TEA_PRICE, 0.001)));
     }
 
     @Test
     public void computes_hot_chocolate_price() {
-        Priceable hotChocolate = new HotChocolate();
-        assertThat(hotChocolate.price(), is(closeTo(1.45, 0.001)));
+        Priceable hotChocolate = BeverageMachine.hotChocolate().make();
+        assertThat(hotChocolate.price(), is(closeTo(HOT_CHOCOLATE_REGULAR_PRICE, 0.001)));
     }
 
     @Test
     public void computes_tea_with_milk_price() {
-        Priceable teaWithMilk = new WithMilk(new Tea());
+        Priceable teaWithMilk = BeverageMachine.tea().withMilk().make();
         assertThat(teaWithMilk.price(), is(closeTo(1.60, 0.001)));
     }
 
     @Test
     public void computes_coffee_with_milk_price() {
-        Priceable coffeeWithMilk = new WithMilk(new Coffee());
+        Priceable coffeeWithMilk = BeverageMachine.coffee().withMilk().make();
         assertThat(coffeeWithMilk.price(), is(closeTo(1.30, 0.001)));
     }
 
     @Test
     public void computes_coffee_with_milk_and_cream_price() {
-        Priceable coffeeWithMilkAndCream = new WithCream(new WithMilk(new Coffee()));
-        assertThat(coffeeWithMilkAndCream.price(), is(closeTo(1.45, 0.001)));
+        Priceable coffeeWithMilkAndCream = BeverageMachine.coffee().withMilk().withCream().make();
+        assertThat(coffeeWithMilkAndCream.price(), is(closeTo(HOT_CHOCOLATE_REGULAR_PRICE, 0.001)));
     }
 
     @Test
     public void computes_hot_chocolate_with_cream_price() {
-        Priceable hotChocolateWithCream = new WithCream(new HotChocolate());
-        assertThat(hotChocolateWithCream.price(),  is(closeTo(1.60, 0.001)));
+        Priceable hotChocolateWithCream = BeverageMachine.hotChocolate().withCream().make();
+        assertThat(hotChocolateWithCream.price(), is(closeTo(1.60, 0.001)));
+    }
+
+    @Test
+    public void computes_tea_with_cream_price()
+    {
+        Priceable hotChocolateWithCream = BeverageMachine.tea().withCream().make();
+        assertThat(hotChocolateWithCream.price(), is(closeTo(1.65, 0.001)));
     }
 }
